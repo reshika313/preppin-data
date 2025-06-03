ba64711f371bf20a3845d7256b20b9918f1c9a57
Reshika Chilakapati <107808909+reshika313@users.noreply.github.com>
2025-06-03 10:51:38 +0100

    PD_2021_W1.sql

--- PD_2021_W1.sql
+++ PD_2021_W1.sql
@@ -1,14 +1,14 @@
 SELECT 
-    ORDER_ID,
+    ORDER_ID, --selecting the existing fields I need for the final output
     CUSTOMER_AGE,
     BIKE_VALUE,
     EXISTING_CUSTOMER,
-    DAY(DATE) AS DAY_OF_MONTH,
-    QUARTER(DATE) AS QUARTER,
-    CAST(SPLIT(STORE_BIKE, ' - ')[0] AS STRING) AS STORE,
+    DAY(DATE) AS DAY_OF_MONTH, --coverting date into day
+    QUARTER(DATE) AS QUARTER, --converting date into quarter
+    CAST(SPLIT(STORE_BIKE, ' - ')[0] AS STRING) AS STORE, --splitting store bike field to create the store field
 
     CASE
-        WHEN SPLIT(STORE_BIKE, ' - ')[1] = 'Graval' THEN 'Gravel'
+        WHEN SPLIT(STORE_BIKE, ' - ')[1] = 'Graval' THEN 'Gravel' --fixing spelling mistakes and creating the bike field by splitting
         WHEN SPLIT(STORE_BIKE, ' - ')[1] = 'Gravle' THEN 'Gravel'
         WHEN SPLIT(STORE_BIKE, ' - ')[1] = 'Rood' THEN 'Road'
         WHEN SPLIT(STORE_BIKE, ' - ')[1] = 'Rowd' THEN 'Road'
@@ -16,6 +16,6 @@ SELECT
         ELSE SPLIT(STORE_BIKE, ' - ')[1]
     END AS BIKE
 
-FROM PD2021_WK01
+FROM PD2021_WK01 --converting order id into an integer and filtering and ordering this column
 WHERE TO_NUMBER(ORDER_ID) > 10
 ORDER BY TO_NUMBER(ORDER_ID);
